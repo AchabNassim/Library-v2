6599c93f5a3430ce7bedcc1292a559223d2613ff
AchabNassim <achab.nassim.solicode@gmail.com>
2022-01-04 16:20:19 +0100

    .

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
 	let title = document.getElementById ("titreForm");
 	var messageTitle = document.getElementById ("messageTitle");
+	var letters = /^[A-Za-z ]+$/;
 
 	let author = document.getElementById ("auteurForm");
 	var messageAuthor = document.getElementById ("messageAuthor");
 
 	let price = document.getElementById ("prixForm");
 	var messagePrice = document.getElementById ("messagePrice");
+	let prix = /^\d+\.?\d*$/
 
 	let date = document.getElementById("dateForm");
 	var messageDate = document.getElementById ("messageDate");
@@ -18,13 +20,31 @@
 	let typeRadio = document.getElementsByClassName("type");
 	var messageType = document.getElementById ("messageType");
 
+	let email = document.getElementById("email")
+	var messageEmail = document.getElementById("messageEmail")
+	let mail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
 	var valide = true;
+	class Book {
+		constructor(title,author,price,date,language,type,email){
+			this.title = title;
+			this.author = author;
+			this.price = price;
+			this.date = date;
+			this.language = language;
+			this.type = type;
+			this.email = email;
+		}
+		DétailOuvrage(){
+			return "l'ouvrage " + this.title + "est un " + this.type + "en langue " + this.language + ", écrit par " + this.author + "et publié le " + this.date + "le prix est de " + this.price
+		}
+	}
 	
 	// Table Section
 
 	var myTable = document.getElementById("table");
-
 	var selectedType = "";
+	let bookList = []
 	// function
 
 function validateForm (e){
@@ -33,23 +53,12 @@ function validateForm (e){
 
 	// title
 
-	if (title.value == ""){
-		messageTitle.innerHTML = "please enter a title"	;
+	if (letters.test(title.value) == false){
+		messageTitle.textContent = "please enter a valid title";
 		return valide
-		
-	}
-	else if (title.value.length <= 5 ){
-		messageTitle.innerHTML = "The minimum is 5 characters";
-		return valide
-	}
-	else if (title.value.length >= 30){
-		messageTitle.innerHTML = "The maximum is 30 characters"	;
-		return valide
-
-		
 	}
 	else {
-		messageTitle.innerHTML = "sent!";
+		messageTitle.textContent = "sent!";
 		document.getElementById("messageTitle").style.color = "green";
 		valide = true
 	}
@@ -57,36 +66,25 @@ function validateForm (e){
 
 	// Author
 
-	if (author.value == ""){
-		messageAuthor.innerHTML = "please enter a name"	;
+	if (letters.test(author.value) == false){
+		messageAuthor.textContent = "please enter a name"	;
 		return valide
-		
-	}
-	else if (author.value.length <= 5 ){
-		messageAuthor.innerHTML = "The minimum is 5 characters"	;
-		return valide
-
-	}
-	else if (author.value.length >= 20){
-		messageAuthor.innerHTML = "The maximum is 20 characters";
-		return valide
-
 	}
 	else {
-		messageAuthor.innerHTML = "sent!";
+		messageAuthor.textContent = "sent!";
 		document.getElementById("messageAuthor").style.color = "green";
 		valide = true
 	}
 
 	// price
 
-	if (isNaN(price.value)  || price.value <= 0){
-		messagePrice.innerHTML = "Please enter a real number";
+	if (prix.test(price.value) == false){
+		messagePrice.textContent = "Please enter a valid number";
 		return valide
 
 	}
 	else {
-		messagePrice.innerHTML = "sent!";
+		messagePrice.textContent = "sent!";
 		document.getElementById("messagePrice").style.color = "green";
 		valide = true
 	}
@@ -94,12 +92,12 @@ function validateForm (e){
 	// date
 
 	if (date.value == ""){
-		messageDate.innerHTML = "Please pick a date";
+		messageDate.textContent = "Please pick a date";
 		return valide	
 
 	}
 	else {
-		messageDate.innerHTML = "sent!";
+		messageDate.textContent = "sent!";
 		document.getElementById("messageDate").style.color = "green";
 		valide = true
 	}
@@ -107,12 +105,12 @@ function validateForm (e){
  	// language 
 
 	if (language.value == ""){
-		messageLangue.innerHTML = "Please choose a language";
+		messageLangue.textContent = "Please choose a language";
 		return valide
 
 	}
 	else {
-		messageLangue.innerHTML = "sent!";
+		messageLangue.textContent = "sent!";
 		document.getElementById("messageLangue").style.color = "green";
 		valide = true
 	}
@@ -123,58 +121,67 @@ function validateForm (e){
     var i = 0;
     while (i < typeRadio.length) {
         if (!typeRadio[i].checked){
-        	messageType.innerHTML = "please pick a type";
+        	messageType.textContent = "please pick a type";
         	valide = false;
         }
         else {
-    	messageType.innerHTML = "Sent!";
+    	messageType.textContent = "Sent!";
     	document.getElementById("messageType").style.color = "green";
     	valide = true
     	break;
     }
         i++;
     }
+
+    // Email
+    if (mail.test(email.value)== false){
+    	messageEmail.textContent = "Please enter a valid Email"
+    	return valide
+    }
+    else {
+    	messageEmail.textContent = "Sent!"
+    	document.getElementById("messageEmail").style.color = "green";
+    	valide = true
+    }
     
     
 
     // Table insertion
 		    
-  		if(valide == true){	
-  		
-	    var newRow = myTable.insertRow();
-	    var cell1 = newRow.insertCell(0);
-	    var cell2 = newRow.insertCell(1);
-	    var cell3 = newRow.insertCell(2);
-	    var cell4 = newRow.insertCell(3);
-	    var cell5 = newRow.insertCell(4);
-	    var cell6 = newRow.insertCell(5);
-	    var cell7 = newRow.insertCell(6);
+  		if(valide == true){
+  			var checkedRadio = document.querySelector('input[name="type"]:checked');
+  			var book = new Book(title.value,author.value,price.value,date.value,language.value,checkedRadio.value,email.value)
+  			bookList.push(book);
+  			//tri
+  			for (var i = 0 ; i < bookList.length;i++){
+  				myTable.innerHTML = ""
+  				var newRow = myTable.insertRow()
+  				newRow.insertCell(0).innerHTML = bookList[i].title;
+  				newRow.insertCell(1).innerHTML = bookList[i].author;
+  				newRow.insertCell(2).innerHTML = bookList[i].price;
+  				newRow.insertCell(3).innerHTML = bookList[i].date;
+  				newRow.insertCell(4).innerHTML = bookList[i].language;
+  				newRow.insertCell(5).innerHTML = bookList[i].type;
+  				newRow.insertCell(6).innerHTML = bookList[i].email;
+  				newRow.insertCell(7).innerHTML =`<input type="submit" value="edit" onclick="edit(this)" id="editBtn">` +
+  												`<input type="submit" value="delete" onclick="Delete(this)" id="deleteBtn">`
 
-	    // cell content
-	    cell1.innerHTML = title.value;
-	    cell2.innerHTML = author.value;
-	    cell3.innerHTML = price.value;
-	    cell4.innerHTML = date.value;
-	    cell5.innerHTML = language.value;
-	    cell7.innerHTML = `<input type="submit" onclick="edit(this)" value="edit" id="editbtn">` +
-	    					`<input type="submit" onclick="Delete(this)" value="delete" id="deletebtn" >`    
-
-	    // type
+  			}
+  	}
+}  			
 	    
-	    for(var j = 0; j < typeRadio.length ; j++){
-	    	if (typeRadio[j].checked){
-	    		selectedType = typeRadio[j].value;
-	    	}
-	    }
-
-	    cell6.innerHTML = selectedType;
-    }
-}
 
 var form = document.getElementsByTagName("form")[0];
 form.addEventListener("submit", validateForm)
 
-	// Delete function
+
+
+
+
+
+
+
+
 
 function Delete(btn){
 	const confirmText = "are you sure you want to delete this row?"
@@ -183,39 +190,3 @@ function Delete(btn){
  	row.parentNode.removeChild(row);
 }
 
-	// edit function
-function edit(r){
-	var i = r.parentNode.parentNode.rowIndex;
-	var row = myTable.rows[i]
-	if (r.value == "edit"){
-		title.value = row.cells[0].innerHTML;
-		author.value = row.cells[1].innerHTML;
-        price.value = row.cells[2].innerHTML;
-        date.value = row.cells[3].innerHTML;
-       	language.value = row.cells[4].innerHTML;
-
-       	// type 
-       	for(var j = 0; j < typeRadio.length ; j++){
-       		if(row.cells[5].innerHTML == typeRadio[j].value){
-       			typeRadio[j].checked = true;
-       		}
-		}
-		// saving function
-		r.value = "save"
-		document.getElementById("submit").setAttribute("disabled", "true");
- }
-		else {
-			row.cells[0].innerHTML = title.value;
-			row.cells[1].innerHTML = author.value;
-			row.cells[2].innerHTML = price.value;
-			row.cells[3].innerHTML = date.value;
-			row.cells[4].innerHTML = language.value
-			for(var j = 0 ; j < typeRadio.length; j++){
-                    if (typeRadio[j].checked){
-                        row.cells[5].innerHTML = typeRadio[j].value;
-                    }
-                }
-           		r.value = "edit";
-                document.getElementById("submit").removeAttribute("disabled")
-		}
-  	}
