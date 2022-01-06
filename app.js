c0288be289fa2d961a478158556cf8cc86bb6315
AchabNassim <achab.nassim.solicode@gmail.com>
2022-01-06 16:11:28 +0100

    .

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 	let title = document.getElementById ("titreForm");
 	var messageTitle = document.getElementById ("messageTitle");
 	var letters = /^[A-Za-z ]+$/;
+	let info = document.getElementById("info")
 
 	let author = document.getElementById ("auteurForm");
 	var messageAuthor = document.getElementById ("messageAuthor");
@@ -36,7 +37,7 @@
 			this.email = email;
 		}
 		DétailOuvrage(){
-			return "l'ouvrage " + this.title + "est un " + this.type + "en langue " + this.language + ", écrit par " + this.author + "et publié le " + this.date + "le prix est de " + this.price
+			return `l'ouvrage ${this.title} est un ${this.type}  en  ${this.language}, écrit par ${this.author} et publié le ${this.date} le prix est de  ${this.price}`
 		}
 	}
 	
@@ -45,12 +46,23 @@
 	var myTable = document.getElementById("table");
 	var selectedType = "";
 	let bookList = []
+
+	// localStorage
+
+	var list = ""
+	list = JSON.parse(localStorage.getItem("list"));
+	if(list != null){
+		for (i= 0 ; i < list.length ; i++){
+			var localBooks = new Book (list[i].titre,list[i].author,list[i].price,list[i].date,list[i].language,list[i].type,list[i].email)
+			bookList.push(localBooks)
+			tri()
+		}
+	}
 	// function
 
 function validateForm (e){
 	e.preventDefault();
 	valide = false;
-
 	// title
 
 	if (letters.test(title.value) == false){
@@ -62,8 +74,6 @@ function validateForm (e){
 		document.getElementById("messageTitle").style.color = "green";
 		valide = true
 	}
-	
-
 	// Author
 
 	if (letters.test(author.value) == false){
@@ -75,7 +85,6 @@ function validateForm (e){
 		document.getElementById("messageAuthor").style.color = "green";
 		valide = true
 	}
-
 	// price
 
 	if (prix.test(price.value) == false){
@@ -88,20 +97,17 @@ function validateForm (e){
 		document.getElementById("messagePrice").style.color = "green";
 		valide = true
 	}
-
 	// date
 
 	if (date.value == ""){
 		messageDate.textContent = "Please pick a date";
 		return valide	
-
 	}
 	else {
 		messageDate.textContent = "sent!";
 		document.getElementById("messageDate").style.color = "green";
 		valide = true
 	}
-
  	// language 
 
 	if (language.value == ""){
@@ -114,9 +120,7 @@ function validateForm (e){
 		document.getElementById("messageLangue").style.color = "green";
 		valide = true
 	}
-
 	// radio buttons
-
 
     var i = 0;
     while (i < typeRadio.length) {
@@ -143,19 +147,27 @@ function validateForm (e){
     	document.getElementById("messageEmail").style.color = "green";
     	valide = true
     }
-    
-    
 
-    // Table insertion
-		    
+    // Table insertion		    
   		if(valide == true){
   			var checkedRadio = document.querySelector('input[name="type"]:checked');
   			var book = new Book(title.value,author.value,price.value,date.value,language.value,checkedRadio.value,email.value)
   			bookList.push(book);
-  			//tri
-  			for (var i = 0 ; i < bookList.length;i++){
-  				myTable.innerHTML = ""
-  				var newRow = myTable.insertRow()
+  			localStorage.setItem("list",JSON.stringify(bookList))
+  			info.textContent = book.DétailOuvrage()
+  			tri()
+  			myTable.innerHTML = ""		
+  			charger()		
+	}  			
+}	    
+
+var form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", validateForm)
+
+
+function charger(){
+  			for (var i = 0 ; i < bookList.length;i++){	
+  				let newRow = myTable.insertRow()
   				newRow.insertCell(0).innerHTML = bookList[i].title;
   				newRow.insertCell(1).innerHTML = bookList[i].author;
   				newRow.insertCell(2).innerHTML = bookList[i].price;
@@ -167,26 +179,63 @@ function validateForm (e){
   												`<input type="submit" value="delete" onclick="Delete(this)" id="deleteBtn">`
 
   			}
-  	}
-}  			
-	    
-
-var form = document.getElementsByTagName("form")[0];
-form.addEventListener("submit", validateForm)
+	}
 
 
-
-
-
-
-
-
+function tri(){
+ 		bookList.sort(function(a,b){
+ 		if( a.author < b.author){
+ 			return -1
+	}
+})
+}
 
 
 function Delete(btn){
 	const confirmText = "are you sure you want to delete this row?"
 	if(confirm(confirmText) == true)
-	var row = btn.parentNode.parentNode;
- 	row.parentNode.removeChild(row);
+	var row = btn.parentNode.parentNode.rowIndex-1;
+ 	bookList.splice(row,1)
+ 	localStorage.setItem("list",JSON.stringify(bookList))
+ 	myTable.innerHTML = ""
+ 	charger()
 }
 
+function edit(btn){
+	var index = btn.parentNode.parentNode.rowIndex
+	if (btn.value == edit){
+			title.value = row.cells[0].innerHTML
+			author.value = row.cells[1].innerHTML
+			price.value = row.cells[2].innerHTML
+			date.value = row.cells[3].innerHTML
+			language.value = row.cells[4].innerHTML
+			for (var i = 0 ; i < typeRadio.length ; i++ ){
+				if (row.cells[5].checked == typeRadio[i].value)
+					typeRadio[i].checked = true;
+			}
+			email.value = row.cells[6].innerHTML
+			btn.value = "save"
+		}
+		
+
+	else {
+            bookList[index-1].title = title.value;
+            bookList[index-1].author = author.value;
+            bookList[index-1].price = price.value;
+            bookList[index-1].date = date.value;
+            bookList[index-1].language = language.value;
+            for(var i=0; i < typeRadio.length ;i++){
+                if(typeRadio[i].checked){
+                    bookList[index-1].Type = typeRadio[i].value;
+                }
+            }
+            bookList[index-1].email = email.value;
+            myTable.innerHTML="";
+            tri()
+            charger();
+            localStorage.setItem("list",JSON.stringify(bookList));
+           	btn.value ="edit";
+	}
+}
+
+charger();
